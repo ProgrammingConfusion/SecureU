a9f071876c5121901e3c14299681c9888fe1cdc3
Leandro dos Santos <dossal1905@gmail.com>
2019-07-03 14:26:44 -0400

    Added form to registration

--- Registration.php
+++ Registration.php
@@ -6,8 +6,45 @@ include "header.php";
 
 ?>
 
+<?php if (isset($_POST["registration"])) {
+    echo "registered";
+} ?>
+
+<h1>Registration</h1>
+
+<form action="registration.php" method="post">
+
+    Email <br>
+    <input type="email" name="email"> <br>
+
+    Username <br>
+    <input type="text" name="username"><br>
+
+    Password <br>
+    <input type="password" name="password"><br>
+
+    Confirm Password <br>
+    <input type="password" name="confirm_password"><br>
+
+    First Name <br>
+    <input type="text" name="first_name"><br>
+
+    Last Name <br>
+    <input type="text" name="last_name"><br>
+    <br>
+
+
+    Register as a
+    <select name="user_role">
+        <option value="student">Student</option>
+        <option value="teacher">Teacher</option>
+    </select><br>
+
+    <input type="submit" name="registration" value="Register">
+
+</form>
+
 
 
 
 <?php include "footer.php"; ?>
-
