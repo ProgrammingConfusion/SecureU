1f0456bef293bbe41fbf6a659aba2df88490c302
Leandro dos Santos <dossal1905@gmail.com>
2019-08-03 12:34:25 -0400

    fixed quiz page to go to next question and result

--- quiz.php
+++ quiz.php
@@ -18,11 +18,22 @@ $attempt_id = 1;
 $quiz_question_num = 2;
 $quiz_question_total = 3;
 
+//default values for page before interaction
+$feedback_icon = "fas fa-edit fa-2x";
+$feedback_message = "";
+//empty string so that response does not display until it is set
+$response = "";
+$button = "Submit";
+//checking if coming to page for the first time
 if (isset($_SESSION["quiz_question_num"])) {
     $quiz_question_num = $_SESSION["quiz_question_num"];
 } else {
+    //if you have just gotten to this page, start from question 1
     $quiz_question_num = 1;
     $_SESSION["quiz_question_num"] = $quiz_question_num;
+    $attempt_score = 0;
+    $_SESSION["attempt_score"] = 0;
+    $_SESSION["attempt_id"] = 1;
 }
 
 
@@ -30,24 +41,51 @@ if (isset($_POST["submit"])) {
 
     $response = $_POST["response"];
     $answer_content = $_SESSION["answer_content"];
+    $correct = 0;
+    $button = "Submit";
+    $quiz_question_id = $_SESSION["quiz_question_id"];
 
+    //checking if submitted response is correct or not
     if (strcasecmp($response, $answer_content) == 0) {
         $correct = 1;
+        $feedback_icon = "fas fa-check-square text-success fa-2x";
+        $feedback_message = "Correct!";
     } else {
         $correct = 0;
+        $feedback_icon = "fas fa-times text-danger fa-2x";
+        $feedback_message = "Incorrect.";
     }
 
+
+    //counting score between questions
+    $_SESSION["attempt_score"] += $correct;
+
+    $sql = "INSERT INTO `quiz_responses` (`response_id`, `response_content`, `response_score`, `quiz_question_id`, `attempt_id`)
+     VALUES (NULL, '', '$correct', '$quiz_question_id', '$attempt_id');";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    //moves to the next question 
+
+}
+
+if (isset($_POST["continue"])) {
     $quiz_question_num += 1;
     $_SESSION["quiz_question_num"] = $quiz_question_num;
 }
-
 if ($quiz_question_num > $quiz_question_total) {
     // because questions are completed
     echo "Finished!";
 
     $quiz_question_num = 1;
     $_SESSION["quiz_question_num"] = 1;
+    header("location:results.php");
 }
+
 
 $sql = "SELECT *
 FROM quiz_questions, quizzes, questions
@@ -65,6 +103,7 @@ if (mysqli_num_rows($result) > 0) {
 
         $question_content = $row["question_content"];
         $_SESSION["answer_content"] = $row["answer_content"];
+        $_SESSION["quiz_question_id"] = $row["quiz_question_id"];
     }
 } else {
     echo "0 results";
@@ -74,6 +113,7 @@ if (mysqli_num_rows($result) > 0) {
 include "header.php";
 ?>
 <!-- page specific styling goes here -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
 <?php include "navbar.php"; ?>
 
@@ -82,19 +122,47 @@ include "header.php";
 <h3>Question <?php echo $quiz_question_num; ?> out of <?php echo $quiz_question_total; ?></h3>
 <h4> <?php echo $question_content; ?></h4>
 <form action="quiz.php" method="post">
-    <input type="text" name="response">
-    <input type="submit" name="submit">
+    <input type="text" name="response" value="<?php echo $response; ?>">
+    <input type="submit" name="submit" value="<?php echo $button; ?>">
+    <input type="submit" name="continue" value="Next Question">
 </form>
+<div><?php echo $feedback_message; ?> </div>
+<span class="<?php echo $feedback_icon; ?>"></span>
+<br>
+<br>
 
-<?php
-if ($correct == 1) {
-    ?>
 
-    <h2>Correct!</h2>
+
+
+
+
+
+
+
+
+<!-- way of progressing through quiz with only one button  -->
+<!-- <h3>Question <?php echo $quiz_question_num; ?> out of <?php echo $quiz_question_total; ?></h3>
+<h4> <?php echo $question_content; ?></h4>
+<form action="quiz.php" method="post">
+    <input type="text" name="response" value="<?php echo $response; ?>">
+    <input type="submit" name="submit" value="<?php echo $button; ?>">
+</form>
+<div><?php echo $feedback_message; ?> </div>
+<span class="<?php echo $feedback_icon; ?>"></span>
+<br>
+<br> -->
+
+
+<!-- Old way of displaying feedback -->
+<!-- <?php
+        if ($correct == 1) {
+            ?>
+
+                                                                                                                                            <h2>Correct! <span class="fas fa-check-square text-success"></span></h2>
 <?php
 } else {
     ?>
-    <h2>Incorrect</h2>
+                                                                                                                                            <h2>Incorrect <span class="fas fa-times text-danger"></span></h2>
 <?php
 }
-?>
+?> -->
