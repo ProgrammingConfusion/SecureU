24f26bb35b02b679a9f7be25afc4339ffd7adc90
Leandro dos Santos <dossal1905@gmail.com>
2019-08-05 20:19:10 -0400

    updated content page to show  content or quiz

--- content.php
+++ content.php
@@ -19,10 +19,22 @@ if (mysqli_num_rows($result) > 0) {
     while ($row = mysqli_fetch_assoc($result)) {
         $content_name = $row["content_name"];
     }
+    $page_title = "$content_name";
+    $content = "content";
 } else {
-    echo "0 results";
+    $sql = "SELECT * FROM `quizzes` WHERE unit_id = $unit_id";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while ($row = mysqli_fetch_assoc($result)) {
+            $quiz_name = $row["quiz_name"];
+        }
+        $page_title = "$quiz_name";
+        $content = "quiz";
+    }
 }
-$page_title = "$content_name";
+
 
 // Code for page goes here
 
@@ -39,23 +51,69 @@ include "header.php";
 <?php include "navbar.php"; ?>
 
 <!-- content for the page starts here -->
+
+
 <?php
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while ($row = mysqli_fetch_assoc($result)) {
-        $content_id = $row["content_id"];
-        $content_name = $row["content_name"];
-        $content_num = $row["content_num"];
-        $content_type = $row["content_type"];
-        $content_code = $row["content_code"];
-        ?>
-        <h1><?php echo $content_name; ?></h1>
+echo $content;
 
-        <p><?php echo $content_code; ?> </p>
+if ($content = "content") {
+    $sql = "SELECT * FROM `content` WHERE unit_id = $unit_id";
+    $result = mysqli_query($conn, $sql);
 
-    <?php
+    if (mysqli_num_rows($result) > 0) {
+        // output data of each row
+        while ($row = mysqli_fetch_assoc($result)) {
+            $content_id = $row["content_id"];
+            $content_name = $row["content_name"];
+            $content_num = $row["content_num"];
+            $content_type = $row["content_type"];
+            $content_code = $row["content_code"];
+            ?>
+            <h1><?php echo $content_name; ?></h1>
+
+            <p><?php echo $content_code; ?> </p>
+
+
+
+        <?php
+        }
     }
 } else {
     echo "0 results";
 }
 ?>
+<div class="container">
+    <?php
+    if ($content = "quiz") {
+        $sql = "SELECT * FROM `quizzes` WHERE unit_id = $unit_id";
+        $result = mysqli_query($conn, $sql);
+
+        if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+            while ($row = mysqli_fetch_assoc($result)) {
+                $quiz_id = $row["quiz_id"];
+                $quiz_name = $row["quiz_name"];
+                $quiz_desc = $row["quiz_desc"];
+                $quiz_type = $row["quiz_type"];
+                $quiz_link = "instructions.php?course_id=$course_id&unit_id=$unit_id&quiz_id=$quiz_id";
+
+                ?>
+                <div class="card">
+                    <div class="card-body ">
+                        <h3 class="card-title"><?php echo $quiz_name; ?></h3>
+                        <p class="card-text"> <?php echo $quiz_desc; ?></p>
+                        <a class="btn btn-primary" href="<?php echo $quiz_link; ?>">Select</a>
+
+                    </div>
+                </div>
+
+
+
+            <?php
+            }
+        }
+    } else {
+        echo "No quiz found.";
+    }
+    ?>
+</div>
