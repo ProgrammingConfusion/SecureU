b9c9993bab294406f8bd716369fb1f9896557368
Leandro dos Santos <dossal1905@gmail.com>
2019-08-18 00:50:16 -0400

    added fileupload and error messages to add content

--- add_content.php
+++ add_content.php
@@ -9,6 +9,9 @@ if (!isset($_SESSION["user_id"])) {
 $page_title = "Add Content";
 
 
+$file_error = 7;
+$error = 7;
+
 if (isset($_POST["add_content"])) {
     require "db_connect.php";
 
@@ -19,13 +22,75 @@ if (isset($_POST["add_content"])) {
     $content_code = mysqli_real_escape_string($conn, trim($_POST["content_code"]));
 
 
-    $sql = "INSERT INTO `content` (`content_id`, `content_name`, `content_num`, `content_type`, `content_code`, `unit_id`)
-     VALUES (NULL, '$content_name', '$content_num', '$content_type', '$content_code', '$unit_id');";
+    if (($_FILES["content_file"]["name"])) {
 
-    if (mysqli_query($conn, $sql)) {
-        echo "New content added successfully";
+
+
+
+        $target_dir = "content_files/";
+        $content_file = $target_dir . basename($_FILES["content_file"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($content_file, PATHINFO_EXTENSION));
+        // Check if image file is a actual image or fake image
+        if (isset($_POST["create_course"])) {
+            $check = getimagesize($_FILES["content_file"]["tmp_name"]);
+            if ($check !== false) {
+                $uploadOk = 1;
+            } else {
+                $uploadOk = 0;
+                $file_error = 1;
+            }
+        }
+        // Check if file already exists
+        if (file_exists($content_file)) {
+            $uploadOk = 0;
+            $file_error = 2;
+        }
+        // Check file size
+        if ($_FILES["content_file"]["size"] > 5000000) {
+            $uploadOk = 0;
+            $file_error = 3;
+        }
+        // Allow certain file formats
+        if (
+            $imageFileType != "pdf"
+        ) {
+            $uploadOk = 0;
+            $file_error = 4;
+        }
+        // Check if $uploadOk is set to 0 by an error
+        if ($uploadOk == 0) {
+
+            // if everything is ok, try to upload file
+        } else {
+            if (move_uploaded_file($_FILES["content_file"]["tmp_name"], $content_file)) {
+                $file_error = 0;
+            } else {
+                $file_error = 5;
+            }
+        }
+        if ($file_error == 0) {
+
+            $sql = "INSERT INTO `content` (`content_id`, `content_name`, `content_num`, `content_type`, `content_code`, `content_file`, `unit_id`)
+            VALUES (NULL, '$content_name', '$content_num', '$content_type', '$content_code', '$content_file', '$unit_id');";
+
+
+            if (mysqli_query($conn, $sql)) {
+                $error = 0;
+            } else {
+                $error = 1;
+            }
+        }
     } else {
-        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+
+        $sql = "INSERT INTO `content` (`content_id`, `content_name`, `content_num`, `content_type`, `content_code`, `content_file`, `unit_id`)
+            VALUES (NULL, '$content_name', '$content_num', '$content_type', '$content_code', '', '$unit_id');";
+
+        if (mysqli_query($conn, $sql)) {
+            $error = 0;
+        } else {
+            $error = 1;
+        }
     }
 }
 
@@ -37,74 +102,166 @@ include "header.php";
 
 <?php // include "navbar.php"; 
 ?>
+<!-- $unit_id = mysqli_real_escape_string($conn, trim($_POST["unit_id"]));
+$content_name = mysqli_real_escape_string($conn, trim($_POST["content_name"]));
+$content_num = mysqli_real_escape_string($conn, trim($_POST["content_num"]));
+$content_type = mysqli_real_escape_string($conn, trim($_POST["content_type"]));
+$content_code = mysqli_real_escape_string($conn, trim($_POST["content_code"])); -->
 
-<form action="add_content.php" method="post">
+<form action="add_content.php" method="post" enctype="multipart/form-data">
 
 
     <h2> Add Content</h2>
 
-    <form action="add_content.php" method="post">
 
-        Select Unit <br>
-        <select name="unit_id" placeholder="Select Unit">
-            <?php
 
-            require "db_connect.php";
+    Select Unit <br>
+    <select name="unit_id" placeholder="Select Unit">
+        <?php
 
-            $user_id = $_SESSION["user_id"];
+        require "db_connect.php";
 
-            $sql = "SELECT * FROM units, courses WHERE units.course_id = courses.course_id AND units.user_id = $user_id ORDER BY unit_name ASC";
-            $result = mysqli_query($conn, $sql);
+        $user_id = $_SESSION["user_id"];
 
-            if (mysqli_num_rows($result) > 0) {
-                // output data of each row
-                while ($row = mysqli_fetch_assoc($result)) {
+        $sql = "SELECT * FROM units, courses WHERE units.course_id = courses.course_id AND units.user_id = $user_id ORDER BY unit_name ASC";
+        $result = mysqli_query($conn, $sql);
 
-                    $unit_id = $row["unit_id"];
-                    $unit_name = $row["unit_name"];
-                    $course_name = $row["course_name"];
+        if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+            while ($row = mysqli_fetch_assoc($result)) {
 
-                    ?>
-            <option value="<?php echo $unit_id ?>"><?php echo "$unit_name - $course_name" ?></option>
+                $unit_id = $row["unit_id"];
+                $unit_name = $row["unit_name"];
+                $course_name = $row["course_name"];
 
-            <?php
-                }
+                ?>
+        <option value="<?php echo $unit_id ?>"><?php echo "$unit_name - $course_name" ?></option>
+
+        <?php
             }
+        }
 
-            ?>
-
-
-
-
-        </select> <br>
-        <br>
-
-
-        <p>Content Name</p>
-        <input type="text" name="content_name"> <br><br>
-
-        <p>Content Order</p>
-        <input type="number" min="1" name="content_num"> <br><br>
-
-        <p>Content Type</p>
-        <select name="content_type"><br>
-
-            <option value="Video">Video</option>
-            <option value="Slide">Slide</option>
-            <option value="PDF">PDF</option>
-
-        </select> <br>
-        <br>
+        ?>
 
 
 
-        <p>Content Source Code</p>
-        <textarea name="content_code" cols="30" rows="10"></textarea> <br>
 
-        <input type="submit" name="add_content" value="Add_content">
+    </select> <br>
+    <br>
 
 
-    </form>
-    <?php
-    include "footer.php";
+    <p>Content Name</p>
+    <input type="text" value="<?php if (isset($_POST['content_name'])) echo $_POST['content_name']; ?>" name="content_name"> <br><br>
+
+    <p>Content Order</p>
+    <input type="number" min="1" name="content_num"> <br><br>
+
+    <p>Content Type</p>
+    <select name="content_type"><br>
+
+        <option value="Video">Video</option>
+        <option value="Slide">Slide</option>
+        <option value="PDF">PDF</option>
+
+    </select> <br>
+    <br>
+
+
+
+    <p>Content Source Code</p>
+    <textarea name="content_code" cols="30" rows="10" onload="myFunction()" id="content_code"> </textarea> <br>
+
+    Content File <br>
+    <input type="file" name="content_file" id="content_file">
+    <br>
+    <br>
+
+    <input type="submit" name="add_content" value="Add_content">
+
+
+</form>
+<br>
+<br>
+<?php if ($file_error == 0) {
     ?>
+<div class="alert alert-success" role="alert">
+    Content Uploaded.
+</div>
+<?php
+
+}
+?>
+<?php if ($file_error == 1) {
+    ?>
+<div class="alert alert-danger" role="alert">
+    File is not a document.
+</div>
+<?php
+
+}
+?>
+<?php if ($file_error == 2) {
+    ?>
+<div class="alert alert-danger" role="alert">
+    Sorry, file already exists.
+</div>
+<?php
+
+}
+?>
+<?php if ($file_error == 3) {
+    ?>
+<div class="alert alert-danger" role="alert">
+    Sorry, your file is too large.
+</div>
+<?php
+
+}
+?>
+<?php if ($file_error == 4) {
+    ?>
+<div class="alert alert-danger" role="alert">
+    Sorry, only PDF files are allowed.
+</div>
+<?php
+
+}
+?>
+<?php if ($file_error == 5) {
+    ?>
+<div class="alert alert-danger" role="alert">
+    Sorry, there was an error uploading your file.
+</div>
+<?php
+
+}
+?>
+
+<?php if ($error == 0) {
+    ?>
+<div class="alert alert-success" role="alert">
+    Content Successfully added.
+</div>
+<?php
+
+}
+?>
+
+<?php if ($error == 1) {
+    ?>
+<div class="alert alert-danger" role="alert">
+    Content could not be added successfully.
+</div>
+<?php
+
+}
+?>
+
+<script>
+    function myFunction() {
+        document.getElementById("content_code").defaultValue = <?php if (isset($_POST['content_code'])) echo $_POST['content_code']; ?>;
+    }
+</script>
+<?php
+include "footer.php";
+?>
